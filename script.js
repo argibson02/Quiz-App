e5b4f20e92003d05d53ee9e5a214755898d4ff0d
Alex Gibson <argibson02@gmail.com>
2021-09-25 12:44:19 -0700

    Switched to an object model over a 2d array for storing Q&As. Seems less complex.

--- script.js
+++ script.js
@@ -1,25 +1,151 @@
 // QUESTION AND ANSWER ARRAYS
-var questionArray01 = ["1.  What color is Garfield?", ["Orange", "White", "Grey", "Blue"]]; //  1
-var questionArray02 = ["2.  What is Garfield’s favorite food?", ["Tuna", "Purina", "Lasagna", "Pizza"]]; //  3
-var questionArray03 = ["3.  What day does Garfield hate?", ["Wednesdays", "Mondays", "Fridays", "Sundays"]]; //  2
-var questionArray04 = ["4.  Who is Garfield’s dog friend?", ["Clive", "Frank", "Odie", "Hale"]]; //  1
-var questionArray05 = ["5.  What kind of dog is Odie?", ["Chihuahua", "German Shepherd", "Labrador", "Beagle"]]; //  4
-var questionArray06 = ["6.  Who is Garfield’s owner?", ["Jon Arbuckle", "Lyman Turner", "Jack Sandman", "Al Murray"]]; //  1
-var questionArray08 = ["8.  What is the name of Garfield’s teddy bear?", ["Teddy", "Billy", "Pooky", "Harry"]]; //  3
-var questionArray09 = ["9.  Which number is Garfield thinking of right now?", ["3", "63", "92", "Lasagna"]]; //   4
-var questionArray10 = ["10. Who is Garfield’s vet?", ["Dr. Julie Payne", "Dr. Liz Wilson", "Dr. Joanna Cauldron", "Dr. Vanessa Marshall"]]; //  2
-var questionArray11 = ["11. Who is Garfield’s nemesis?", ["Nermal", "Arlene", "Odie", "Dr. Liz Wilson"]]; //  1
-var questionArray12 = ["12. What color is Nermal?", ["Pink", "Grey", "White", "Calico"]]; //  2
-var questionArray13 = ["13. How many live-action Garfield movies are there?", ["1", "2", "3", "4"]]; //  2
-var questionArray14 = ["14. How many panels are in a normal Garfield strip?", ["3", "4", "5", "6"]]; //  1
-var questionArray15 = ["15. How many panels are in a Sunday Garfield strip? (Not including the header panel)", ["6", "7", "8", "9"]]; //  2
-var questionArray16 = ["16. How old is Garfield in real-world years?", ["24", "36", "43", "48"]]; //  3
-var questionArray17 = ["17. Which cat has a crush on Garfield?", ["Arlene", "Fluffy", "Ivy", "Abigail"]]; //  1
-var questionArray18 = ["18. What color is Arlene?", ["Grey", "White", "Calico", "Pink"]]; //  4
-var questionArray19 = ["19.  Can Jon understand what Garfield says?", ["Yes", "No", "Sometimes", "It's not canonically defined"]]; //  2
-var questionArray20 = ["20. What has Garfield given up on?", ["Mousing", "Mondays", "Diets", "All of the above"]]; //  4
-var questionArray21 = ["21. Why does Garfield hate Mondays???? Garfield is a cat. Every day is the same for him.", ["I know, right?", "I know, right?", "I know, right?", "I know, right?"]];  // 5
 
+var questionObject01 = [
+    {
+        question: "1.  What color is Garfield?",
+        choices: ["Orange", "White", "Grey", "Blue"],
+        answer: 0,
+    },
+    {
+        question: "2.  What is Garfield’s favorite food?",
+        choices: ["Tuna", "Purina", "Lasagna", "Pizza"],
+        answer: 3,
+    },
+    {
+        question: "3.  What day does Garfield hate?",
+        choices: ["Wednesdays", "Mondays", "Fridays", "Sundays"],
+        answer: 3,
+    },
+    {
+        question: "4.  Who is Garfield’s dog friend?",
+        choices: ["Clive", "Frank", "Odie", "Hale"],
+        answer: 2,
+    },
+    {
+        question: "5.  What kind of dog is Odie?",
+        choices: ["Chihuahua", "German Shepherd", "Labrador", "Beagle"],
+        answer: 3,
+    },
+    {
+        question: "6.  Who is Garfield’s owner?",
+        choices: ["Jon Arbuckle", "Lyman Turner", "Jack Sandman", "Al Murray"],
+        answer: 0,
+    },
+    {
+        question: "8.  What is the name of Garfield’s teddy bear?",
+        choices: ["Teddy", "Billy", "Pooky", "Harry"],
+        answer: 2,
+    },
+    {
+        question: "9.  Which number is Garfield thinking of right now?",
+        choices: ["3", "63", "92", "Lasagna"],
+        answer: 3,
+    },
+    {
+        question: "10. Who is Garfield’s vet?",
+        choices: ["Dr. Julie Payne", "Dr. Liz Wilson", "Dr. Joanna Cauldron", "Dr. Vanessa Marshall"],
+        answer: 1,
+    },
+    {
+        question: "11. Who is Garfield’s nemesis?",
+        choices: ["Nermal", "Arlene", "Odie", "Dr. Liz Wilson"],
+        answer: 0,
+    },
+    {
+        question: "12. What color is Nermal?",
+        choices: ["Pink", "Grey", "White", "Calico"],
+        answer: 1,
+    },
+    {
+        question: "13. How many live-action Garfield movies are there?",
+        choices: ["1", "2", "3", "4"],
+        answer: 1,
+    },
+    {
+        question: "14. How many panels are in a normal Garfield strip?",
+        choices: ["3", "4", "5", "6"],
+        answer: 0,
+    },
+    {
+        question: "15. How many panels are in a Sunday Garfield strip? (Not including the header panel)",
+        choices: ["6", "7", "8", "9"],
+        answer: 1,
+    },
+    {
+        question: "16. How old is Garfield in real-world years?",
+        choices: ["24", "36", "43", "48"],
+        answer: 2,
+    },
+    {
+        question: "17. Which cat has a crush on Garfield?",
+        choices: ["Arlene", "Fluffy", "Ivy", "Abigail"],
+        answer: 0,
+    },
+    {
+        question: "18. What color is Arlene?",
+        choices: ["Grey", "White", "Calico", "Pink"],
+        answer: 3,
+    },
+    {
+        question: "19.  Can Jon understand what Garfield says?",
+        choices: ["Yes", "No", "Sometimes", "It's not canonically defined"],
+        answer: 1,
+    },
+    {
+        question: "20. What has Garfield given up on?",
+        choices: ["Mousing", "Mondays", "Diets", "All of the above"],
+        answer: 3,
+    },
+    {
+        question: "21. Why does Garfield hate Mondays???? Garfield is a cat. Every day is the same for him.",
+        choices: ["I know, right?", "I know, right?", "I know, right?", "I know, right?"],
+        answer: 1,
+    },
+]
+
+
+
+
+//var questionArray01 = ["1.  What color is Garfield?", ["Orange", "White", "Grey", "Blue"]]; //  1
+//var questionArray02 = ["2.  What is Garfield’s favorite food?", ["Tuna", "Purina", "Lasagna", "Pizza"]]; //  3
+//var questionArray03 = ["3.  What day does Garfield hate?", ["Wednesdays", "Mondays", "Fridays", "Sundays"]]; //  2
+//var questionArray04 = ["4.  Who is Garfield’s dog friend?", ["Clive", "Frank", "Odie", "Hale"]]; //  1
+//var questionArray05 = ["5.  What kind of dog is Odie?", ["Chihuahua", "German Shepherd", "Labrador", "Beagle"]]; //  4
+//var questionArray06 = ["6.  Who is Garfield’s owner?", ["Jon Arbuckle", "Lyman Turner", "Jack Sandman", "Al Murray"]]; //  1
+//var questionArray08 = ["8.  What is the name of Garfield’s teddy bear?", ["Teddy", "Billy", "Pooky", "Harry"]]; //  3
+//var questionArray09 = ["9.  Which number is Garfield thinking of right now?", ["3", "63", "92", "Lasagna"]]; //   4
+//var questionArray10 = ["10. Who is Garfield’s vet?", ["Dr. Julie Payne", "Dr. Liz Wilson", "Dr. Joanna Cauldron", "Dr. Vanessa Marshall"]]; //  2
+//var questionArray11 = ["11. Who is Garfield’s nemesis?", ["Nermal", "Arlene", "Odie", "Dr. Liz Wilson"]]; //  1
+//var questionArray12 = ["12. What color is Nermal?", ["Pink", "Grey", "White", "Calico"]]; //  2
+//var questionArray13 = ["13. How many live-action Garfield movies are there?", ["1", "2", "3", "4"]]; //  2
+//var questionArray14 = ["14. How many panels are in a normal Garfield strip?", ["3", "4", "5", "6"]]; //  1
+//var questionArray15 = ["15. How many panels are in a Sunday Garfield strip? (Not including the header panel)", ["6", "7", "8", "9"]]; //  2
+//var questionArray16 = ["16. How old is Garfield in real-world years?", ["24", "36", "43", "48"]]; //  3
+//var questionArray17 = ["17. Which cat has a crush on Garfield?", ["Arlene", "Fluffy", "Ivy", "Abigail"]]; //  1
+//var questionArray18 = ["18. What color is Arlene?", ["Grey", "White", "Calico", "Pink"]]; //  4
+//var questionArray19 = ["19.  Can Jon understand what Garfield says?", ["Yes", "No", "Sometimes", "It's not canonically defined"]]; //  2
+//var questionArray20 = ["20. What has Garfield given up on?", ["Mousing", "Mondays", "Diets", "All of the above"]]; //  4
+//var questionArray21 = ["21. Why does Garfield hate Mondays???? Garfield is a cat. Every day is the same for him.", ["I know, right?", "I know, right?", "I know, right?", "I know, right?"]];  // 5
+
+
+
+
+// var qarry = [
+//     {
+//         question: "q1 ",
+//         choice: ["a","b","rc","d"],
+       
+//         answer: 2
+//     },
+//     {
+//         question: "q1 ",
+//         choice: ["ra","b","c","d"],
+//         answer: 0
+//     }
+// ]
+
+localStorage.setItem("time",value)
+localStorage.getItem("time")
 var answerKeyArray = ["1", "3", "2", "1", "4", "1", "3", "4", "2", "1", "2", "2", "1", "2", "3", "1", "4", "5", "4", "1"]; //  
 var answerKeyArray2d = ["0", "2", "1", "0", "3", "0", "2", "3", "1", "0", "1", "1", "0", "1", "2", "0", "3", "1", "3", "5"]; //  
 
