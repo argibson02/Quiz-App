8c2e2070a125438fde805f77b294508350b3a829
Alex Gibson <argibson02@gmail.com>
2021-09-26 21:57:43 -0700

    Added timer logic. Set out some other global variables.

--- script.js
+++ script.js
@@ -1,131 +1,188 @@
-var questionObject01 = [
+/*Global Variables, */
+/*Questions Variables*/
+var questionsObject = [
     {
-        question: "1.  What color is Garfield?",
+        question: "What color is Garfield?",
         choices: ["Orange", "White", "Grey", "Blue"],
         answer: 0,
     },
     {
-        question: "2.  What is Garfield’s favorite food?",
+        question: "What is Garfield’s favorite food?",
         choices: ["Tuna", "Purina", "Lasagna", "Pizza"],
         answer: 3,
     },
     {
-        question: "3.  What day does Garfield hate?",
+        question: "What day does Garfield hate?",
         choices: ["Wednesdays", "Mondays", "Fridays", "Sundays"],
         answer: 3,
     },
     {
-        question: "4.  Who is Garfield’s dog friend?",
+        question: "Who is Garfield’s dog friend?",
         choices: ["Clive", "Frank", "Odie", "Hale"],
         answer: 2,
     },
     {
-        question: "5.  What kind of dog is Odie?",
+        question: "What kind of dog is Odie?",
         choices: ["Chihuahua", "German Shepherd", "Labrador", "Beagle"],
         answer: 3,
     },
     {
-        question: "6.  Who is Garfield’s owner?",
+        question: "Who is Garfield’s owner?",
         choices: ["Jon Arbuckle", "Lyman Turner", "Jack Sandman", "Al Murray"],
         answer: 0,
     },
     {
-        question: "8.  What is the name of Garfield’s teddy bear?",
+        question: "What is the name of Garfield’s teddy bear?",
         choices: ["Teddy", "Billy", "Pooky", "Harry"],
         answer: 2,
     },
     {
-        question: "9.  Which number is Garfield thinking of right now?",
+        question: "Which number is Garfield thinking of right now?",
         choices: ["3", "63", "92", "Lasagna"],
         answer: 3,
     },
     {
-        question: "10. Who is Garfield’s vet?",
+        question: "Who is Garfield’s vet?",
         choices: ["Dr. Julie Payne", "Dr. Liz Wilson", "Dr. Joanna Cauldron", "Dr. Vanessa Marshall"],
         answer: 1,
     },
     {
-        question: "11. Who is Garfield’s nemesis?",
+        question: "Who is Garfield’s nemesis?",
         choices: ["Nermal", "Arlene", "Odie", "Dr. Liz Wilson"],
         answer: 0,
     },
     {
-        question: "12. What color is Nermal?",
+        question: "What color is Nermal?",
         choices: ["Pink", "Grey", "White", "Calico"],
         answer: 1,
     },
     {
-        question: "13. How many live-action Garfield movies are there?",
+        question: "How many live-action Garfield movies are there?",
         choices: ["1", "2", "3", "4"],
         answer: 1,
     },
     {
-        question: "14. How many panels are in a normal Garfield strip?",
+        question: "How many panels are in a normal Garfield strip?",
         choices: ["3", "4", "5", "6"],
         answer: 0,
     },
     {
-        question: "15. How many panels are in a Sunday Garfield strip? (Not including the header panel)",
+        question: "How many panels are in a Sunday Garfield strip? (Not including the header panel)",
         choices: ["6", "7", "8", "9"],
         answer: 1,
     },
     {
-        question: "16. How old is Garfield in real-world years?",
+        question: "How old is Garfield in real-world years?",
         choices: ["24", "36", "43", "48"],
         answer: 2,
     },
     {
-        question: "17. Which cat has a crush on Garfield?",
+        question: "Which cat has a crush on Garfield?",
         choices: ["Arlene", "Fluffy", "Ivy", "Abigail"],
         answer: 0,
     },
     {
-        question: "18. What color is Arlene?",
+        question: "What color is Arlene?",
         choices: ["Grey", "White", "Calico", "Pink"],
         answer: 3,
     },
     {
-        question: "19.  Can Jon understand what Garfield says?",
+        question: "Can Jon understand what Garfield says?",
         choices: ["Yes", "No", "Sometimes", "It's not canonically defined"],
         answer: 1,
     },
     {
-        question: "20. What has Garfield given up on?",
+        question: "What has Garfield given up on?",
         choices: ["Mousing", "Mondays", "Diets", "All of the above"],
         answer: 3,
     },
     {
-        question: "21. Why does Garfield hate Mondays???? Garfield is a cat. Every day is the same for him.",
+        question: "Why does Garfield hate Mondays???? Garfield is a cat. Every day is the same for him.",
         choices: ["I know, right?", "I know, right?", "I know, right?", "I know, right?"],
         answer: 1,
     },
 ]
 
+var answerKeyArray2 = [0, 2, 1, 0, 3, 0, 2, 3, 1, 0, 1, 1, 0, 1, 2, 0, 3, 1, 3, 5];
+
+
+
+/*Start section variables*/
+var startButton = document.querySelector("#startButton");
+var initials = document.getElementById("initialText");
+
+
+
+/*Choice section variables*/
+var timeCount = document.getElementById("timerNumber");
+///var scoreTime = 
+
+
+
+console.log(questionsObject.length + " qobject01"); // DELETE UPON COMPLETION 
+console.log(answerKeyArray2 + " key2"); // DELETE UPON COMPLETION 
+
+
+
+
+/*Functions for game*/
+function runGame () {
+    timerNumber()
+    
+}
+
+
+
+
+/*---------SAVING INITIALS AND SCORE?-WIP--------------------------------------------------------------------------------------------------------------------*/
+/*see #25*/
+function saveInitials() {
+    // Save related form data as an object
+    var scoreInitials = {
+      initials: initials.value.trim(),
+      time: time.value,
+    };
+    // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+    localStorage.setItem("saveInitials", JSON.stringify(saveInitials));
+  }
+
+/*----------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/*---------SAVING SCORE?-WIP--------------------------------------------------------------------------------------------------------------------*/
+function showScores() {
+    var score = JSON.parse(localStorage.getItem("timeScore"))
+}
+
+
+
+
+/*----------TIMER   WIP-------------------------------------------------------------------------------------------------------------------------------------*/
+/*SEE 10, COUNT DOWN IS WORKING*/
+function timerNumber() {
+    var timeLeft = 61;
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        timeCount.textContent = timeLeft;
+        
+        if (timeLeft === 0) {
+            clearInterval(timeInterval);
+        /*go to score screen */
+        }
+        //else if { //question numer is great than 20, go to scoreboard*/
+        //    localStorage.getItem(timeLeft)
+    //}
+    }, 1000);
+  }
+timerNumber()
+/*----------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 
 
 
 
+// event listener for game start
+startButton.addEventListener("click", runGame);
 
-
-var answerKeyArray = ["1", "3", "2", "1", "4", "1", "3", "4", "2", "1", "2", "2", "1", "2", "3", "1", "4", "5", "4", "1"];
-var answerKeyArray2 = ["0", "2", "1", "0", "3", "0", "2", "3", "1", "0", "1", "1", "0", "1", "2", "0", "3", "1", "3", "5"];
-
-var answerKeyArrayLength = 0;
-answerKeyArrayLength = answerKeyArray.length;
-
-console.log(questionObject01.length + "qobject01");
-
-console.log(answerKeyArray + "key1");
-console.log(answerKeyArray2 + "key2");
-
-
-// expected output: 4
 
 // localStorage.setItem("time", value)
 // localStorage.getItem("time")
-
-// for each, let array point 0 be the question
-// for each let 1 be 1, 2 be 2....
-// corerct answer is is
-
